09e18f88012cd98e2699e8fccbec1ef4539b706d
David <david.arabis@eversource.com>
2021-05-12 21:47:52 +0200

    Added a checkbox component and styled it with ES styles.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import React from 'react';
 // import './App.css';
 import './app.scss';
+import Header from './components/header/Header';
+
+// All the Form components from Carbon
+import { Checkbox }  from 'carbon-components-react';
+import { Form } from 'carbon-components-react';
+import { FormGroup } from 'carbon-components-react';
+import { FileUploader } from 'carbon-components-react';
+import { NumberInput } from 'carbon-components-react';
+import { RadioButton } from 'carbon-components-react';
+import { RadioButtonGroup } from 'carbon-components-react';
 import { Button } from 'carbon-components-react';
+import { Search } from 'carbon-components-react';
+import { Select } from 'carbon-components-react';
+import { SelectItem } from 'carbon-components-react';
+import { TextInput } from 'carbon-components-react';
+import { Toggle } from 'carbon-components-react';
+import { mdx } from 'carbon-components-react';
+
+// import { Button } from 'carbon-components-react';
 
 function App() {
+
+    const checkboxEvents = {
+        className: 'some-class',
+        labelText: 'Checkbox label',
+    };
+
+    const fieldsetCheckboxProps = () => ({
+        className: 'some-class',
+        // legendText: 'Fieldset Legend Text',
+        // message: boolean('Show form requirement (message)', false),
+        messageText: 'Message Text',
+        // invalid: boolean('Mark as invalid (invalid)', false),
+    });
+
   return (
     <div className="App">
         <div className="o-container">
-            <Button>Example usage</Button>
+            <Header />
+            <FormGroup {...fieldsetCheckboxProps()}>
+                <Checkbox defaultChecked {...checkboxEvents} id="checkbox-0" />
+                <Checkbox {...checkboxEvents} id="checkbox-1" />
+            </FormGroup>
+            <Search />
+
+            <Button />
         </div>
     </div>
   );
---
+++ src/components/header/Header.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Header() {
+    return <h1 className="mb-5">Carbon Components in React</h1>;
+}
+
+export default Header;
